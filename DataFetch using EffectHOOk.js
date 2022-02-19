268b3a7ac704f0678b563d791ffe29bc19bb1dc4
Noman Hasan <48345904+NomanHasanNM@users.noreply.github.com>
2022-02-19 00:16:58 -0800

    Add files via upload

---
+++ DataFetch using EffectHOOk.js
@@ -0,0 +1,52 @@
+/////Appp.js
+
+
+import React from 'react'
+import DataFetch from './useeffect/DataFetch'
+
+
+ export default function App() {
+  
+  return(
+    
+        <div>
+      <DataFetch/>
+    </div>
+  )
+  }
+
+
+/////DataFetch.js
+
+import React, {useEffect,useState } from 'react'
+
+const DataFetch = () => {
+
+    const [todos, setTodos] = useState(null);
+   useEffect(() => {
+       fetch("https://jsonplaceholder.typicode.com/todos")
+       .then((res) => {
+         return res.json();
+       })
+
+       .then((data) =>{
+           setTodos(data);
+        
+       });
+    
+   }, []);
+
+  return (
+    <div>
+    <h1> Todos</h1>
+    {
+        todos && todos.map((todo) =>{
+            return<p key={todo.id}> {todo.title} </p>
+
+        })}
+   
+    </div>
+  );
+};
+
+export default DataFetch
